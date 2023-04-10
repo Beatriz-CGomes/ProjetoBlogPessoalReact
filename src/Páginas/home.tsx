518f80c934ec94cad0ff057038a78279ca5c6497
Beatriz-CGomes <Beatriz.cg15@hotmail.com>
2023-04-10 17:07:59 -0300

    React

---
+++ src/Páginas/home.tsx
@@ -0,0 +1,14 @@
+function Home() {
+    return
+    <>
+        <h2>Hello Word </h2>
+        <ul>
+            <li>1 - Iniciando o projeto com React</li>
+            <li>2 - Quebrando a maldição hihi</li>
+        </ul>
+
+    </>
+}
+
+
+export default Home
--- src/assets/home.tsx
+++
@@ -1,5 +0,0 @@
-function Home() {
-    return <>
-        <h2>Hello Word </h2>
-    </>
-}
